c766aa1ca74e2aeb88745dc5ca092f4503a7421c
cryptus-neoxys <sharma.dev4242@gmail.com>
2020-12-08 22:01:14 +0530

    Link List

---
+++ src/components/LinkList.js
@@ -0,0 +1,22 @@
+import LinkCard from "./LinkCard";
+
+export default function LinkList({ links, refreshLinks }) {
+  return (
+    <div>
+      <h2 className="my-4">Links</h2>
+      {links &&
+        links
+          .filter((link) => !link.archived)
+          .map((link) => (
+            <LinkCard key={link.id} link={link} refreshLinks={refreshLinks} />
+          ))}
+      <h2 className="my-4">Archived</h2>
+      {links &&
+        links
+          .filter((link) => link.archived)
+          .map((link) => (
+            <LinkCard key={link.id} link={link} refreshLinks={refreshLinks} />
+          ))}
+    </div>
+  );
+}
